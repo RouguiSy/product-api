62cba5afdaf9484905305dc1f86e94be02062d11
RouguiSy <rougui080601@gmail.com>
2026-03-15 01:31:27 +0000

    feat: create product-api REST controller

---
+++ src/main/java/sn/isi/l3gl/api/ProductApiApplication.java
@@ -0,0 +1,20 @@
+package sn.isi.l3gl.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {
+        "sn.isi.l3gl.api",
+        "sn.isi.l3gl.core"
+})
+@EntityScan(basePackages = "sn.isi.l3gl.core.entity")
+@EnableJpaRepositories(basePackages = "sn.isi.l3gl.core.repository")
+public class ProductApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApiApplication.class, args);
+    }
+}
